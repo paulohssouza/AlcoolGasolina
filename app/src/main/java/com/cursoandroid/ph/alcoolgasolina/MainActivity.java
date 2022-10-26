982a72392f83942b8bc5ae91a8d5e41bbb9ea6af
Paulo Henrique <34384560+paulohssouza@users.noreply.github.com>
2022-10-25 21:16:18 -0300

    App Alcool ou Gasolina

---
+++ app/src/main/java/com/cursoandroid/ph/alcoolgasolina/MainActivity.java
@@ -0,0 +1,49 @@
+package com.cursoandroid.ph.alcoolgasolina;
+
+import android.support.design.widget.TextInputEditText;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextInputEditText inputAlcool, inputGas;
+    private TextView textResult;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        inputAlcool = findViewById(R.id.inputAlcool);
+        inputGas = findViewById(R.id.inputGas);
+        textResult = findViewById(R.id.textViewResult);
+    }
+
+    public void calcBestPrice (View view) {
+        String priceAlcool = inputAlcool.getText().toString();
+        String priceGas = inputGas.getText().toString();
+        if (!validateInputs(priceAlcool, priceGas)) {
+            textResult.setText("Digite todos os valores.");
+        } else {
+            double priceAlc = Double.parseDouble(priceAlcool);
+            double priceG = Double.parseDouble(priceGas);
+            if (priceAlc / priceG >= 0.7) {
+                textResult.setText("Melhor utilizar gasolina.");
+            } else {
+                textResult.setText("Melhor utilizar álcool.");
+            }
+        }
+    }
+
+    public boolean validateInputs (String priceAlcool, String priceGas) {
+        boolean validate = true;
+        if(priceAlcool == null || priceGas.equals("")) {
+           validate = false;
+        } else if (priceGas == null || priceGas.equals("")){
+            validate = false;
+        }
+        return validate;
+    }
+
+}
